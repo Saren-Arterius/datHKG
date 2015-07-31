57bc8cb98924665468c842a7fb5b57b5899ae3e7
Saren Arterius <saren@wtako.net>
2015-07-31 20:11:19 +0800

    pep 8

--- remove-ad.py
+++ remove-ad.py
@@ -7,17 +7,17 @@ from subprocess import call
 
 SCRIPT_PATH = dirname(realpath(__file__))
 JSON_NAME = "owned-sku.json"
-JSON_PATH = join(SCRIPT_PATH, JSON_NAME);
+JSON_PATH = join(SCRIPT_PATH, JSON_NAME)
 SKU_NAME = "ad_removal"
 
 if __name__ == "__main__":
-    data = loads(open(JSON_PATH).read())
-    if len(argv) == 2:
-        data[SKU_NAME].append(int(argv[1]))
-    data[SKU_NAME].sort()
-    with open(JSON_PATH, "w") as file:
-        file.write(dumps(data))
-    chdir(SCRIPT_PATH)
-    call(["git", "add", JSON_NAME])
-    call(["git", "commit", "-m", "Added user ID"])
-    call(["git", "push", "origin", "master"])
+  data = loads(open(JSON_PATH).read())
+  if len(argv) == 2:
+    data[SKU_NAME].append(int(argv[1]))
+  data[SKU_NAME].sort()
+  with open(JSON_PATH, "w") as file:
+    file.write(dumps(data))
+  chdir(SCRIPT_PATH)
+  call(["git", "add", JSON_NAME])
+  call(["git", "commit", "-m", "Added user ID"])
+  call(["git", "push", "origin", "master"])
